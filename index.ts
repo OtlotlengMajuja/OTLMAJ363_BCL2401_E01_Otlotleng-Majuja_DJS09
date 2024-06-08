b2e9c10cabfe36c3e6d0ddcb8315ade183b0346a
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-08 15:01:12 +0200

    Added the properties

--- index.ts
+++ index.ts
@@ -56,8 +56,49 @@ const properties: {
   };
   contact: string;
   isAvailable: boolean;
-}[] =
-  // Functions
-  showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+}[] = [
+  {
+    image: "",
+    title: "Colombian Shack",
+    price: 45,
+    location: {
+      firstLine: "shack 37",
+      city: "Bogota",
+      code: 45632,
+      country: "Colombia",
+    },
+    contact: "marywinkle@gmail.com",
+    isAvailable: true,
+  },
+  {
+    image: "",
+    title: "Polish Cottage",
+    price: 34,
+    location: {
+      firstLine: "no 23",
+      city: "Gdansk",
+      code: 343903,
+      country: "Poland",
+    },
+    contact: "garydavis@hotmail.com",
+    isAvailable: false,
+  },
+  {
+    image: "",
+    title: "London Flat",
+    price: 23,
+    location: {
+      firstLine: "flat 15",
+      city: "London",
+      code: 35433,
+      country: "United Kingdom",
+    },
+    contact: "andyluger@aol.com",
+    isAvailable: true,
+  },
+];
+
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 
 populateUser(you.isReturning, you.firstName);
