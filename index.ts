56f2e23d9b79d1fa79b45b02a02e590f4b2edaf7
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-08 16:52:06 +0200

    Included the images for each property

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+const propertyContainer = document.querySelector(".properties");
+
 import { showReviewTotal, populateUser } from "./utils";
 let isOpen: boolean;
 
@@ -43,7 +45,7 @@ const you: {
   stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
 };
 
-// Properties
+// Array of  Properties
 const properties: {
   image: string;
   title: string;
@@ -58,7 +60,7 @@ const properties: {
   isAvailable: boolean;
 }[] = [
   {
-    image: "",
+    image: "images/colombia-property.jpg",
     title: "Colombian Shack",
     price: 45,
     location: {
@@ -71,7 +73,7 @@ const properties: {
     isAvailable: true,
   },
   {
-    image: "",
+    image: "images/poland-property.jpg",
     title: "Polish Cottage",
     price: 34,
     location: {
@@ -84,7 +86,7 @@ const properties: {
     isAvailable: false,
   },
   {
-    image: "",
+    image: "images/london-property.jpg",
     title: "London Flat",
     price: 23,
     location: {
@@ -102,3 +104,5 @@ const properties: {
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 
 populateUser(you.isReturning, you.firstName);
+
+// Add the properties
