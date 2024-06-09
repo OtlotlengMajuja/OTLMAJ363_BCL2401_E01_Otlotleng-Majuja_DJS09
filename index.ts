1bd13d9ee2117ff4dc8dece55199b52a1113b540
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-06-09 18:14:05 +0200

    Replaced the redundant code with an interface

--- index.ts
+++ index.ts
@@ -9,8 +9,15 @@ const footer = document.querySelector(".footer");
 
 let isLoggedIn: boolean;
 
+interface Review {
+  name: string;
+  stars: number;
+  loyaltyUser: LoyaltyUser;
+  date: string;
+}
+
 // Reviews
-const reviews: any[] = [
+const reviews: Review[] = [
   {
     name: "Sheia",
     stars: 5,
